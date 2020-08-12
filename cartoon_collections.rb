631c313f24fa5e8a7282bdc9702c2002a8509c8a
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-12 23:30:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -27,7 +27,7 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
  
   planeteer_calls.count do |call|
-    if valid_calls.include? |call|
+    if valid_calls.include? call
       call
     end
   end
