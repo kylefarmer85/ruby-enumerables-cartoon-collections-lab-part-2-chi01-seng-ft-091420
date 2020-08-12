1edb2598bf57931d3af091caaad8107ee992fcb5
Kyle Farmer <kylefarmer85@gmail.com>
2020-08-12 22:39:05 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(planeteer_calls)
   # Return a new array of the results
   
   array.map do |string| 
-    "#{string.capitalize}!" }
+    "#{string.capitalize}!" 
   end
 end
 
